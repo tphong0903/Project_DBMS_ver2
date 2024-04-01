15750c5d70ceb72274a320079cb294de78120c0d
TPhong <phongga088@gmail.com>
2024-04-01 20:18:19 +0700

    add  luu hinh anh

--- Project_DMS/Project_ver1/Properties/Resources.Designer.cs
+++ Project_DMS/Project_ver1/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Project_ver1.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon logo {
-            get {
-                object obj = ResourceManager.GetObject("logo", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }
--- Project_DMS/Project_ver1/UI/Detail/SPDetail.cs
+++ Project_DMS/Project_ver1/UI/Detail/SPDetail.cs
@@ -6,8 +6,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,13 +23,15 @@ namespace Project_ver1.UI
         DataTable dtSanPham = null;
         int Check;
         string Product_Id;
-        public SPDetail(int check,string Product_ID)
+        Image img=  null;
+        string selectedFilePath = null;
+        public SPDetail(int check, string Product_ID)
         {
-            Check=check;
+            Check = check;
             Product_Id = Product_ID;
-            
+
             InitializeComponent();
-            MaSP.ReadOnly= true;
+            MaSP.ReadOnly = true;
             TenSP.ReadOnly = true;
             Gia.ReadOnly = true;
             ThuongHieu.ReadOnly = true;
@@ -37,17 +41,19 @@ namespace Project_ver1.UI
             imgBtn.Visible = false;
             SaveButton.Visible = false;
             this.Text = "Chi tiết sản phẩm";
+        
             if (Check == 1)
             {
+                PicProduct.Image = GetImageByName("AdidasXCrazyfastMessi.jpg");
                 this.Text = "Chi tiết sản phẩm";
                 MaSP.ReadOnly = false;
                 TenSP.ReadOnly = false;
                 Gia.ReadOnly = false;
                 ThuongHieu.ReadOnly = true;
                 DanhMuc.ReadOnly = true;
-                SaveButton.Visible= true;
+                SaveButton.Visible = true;
             }
-            else if(Check == 2) 
+            else if (Check == 2)
             {
                 this.Text = "Thêm sản phẩm";
                 MaSP.ReadOnly = false;
@@ -61,7 +67,7 @@ namespace Project_ver1.UI
                 imgBtn.Visible = true;
             }
             dbsp = new DBSanPham();
-         
+
         }
         public void LoadData()
         {
@@ -90,30 +96,79 @@ namespace Project_ver1.UI
                 MessageBox.Show("Vui lòng chọn một hàng");
                 this.Close();
             }
-            else if (Check == 1 || Check ==0)
+            else if (Check == 1 || Check == 0)
                 LoadData();
         }
 
         private void SaveButton_Click(object sender, EventArgs e)
         {
+            if (img != null && !string.IsNullOrEmpty(selectedFilePath))
+            {
+                // Đường dẫn tới thư mục IMG trong thư mục Image của project_ver1
+                string imgFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../Project_ver1/UI/Image");
 
+                // Tạo thư mục IMG nếu nó không tồn tại
+                if (!Directory.Exists(imgFolderPath))
+                {
+                    Directory.CreateDirectory(imgFolderPath);
+                }
+                // Tạo tên file từ đường dẫn đã chọn
+                string imgFileName = Path.GetFileName(selectedFilePath);
+                // Đường dẫn đến tệp tin ảnh trong thư mục IMG
+                string imgFilePath = Path.Combine(imgFolderPath, imgFileName);
+                // Lưu ảnh vào thư mục IMG
+                img.Save(imgFilePath);
+                // Hiển thị thông báo thành công
+                MessageBox.Show("Ảnh đã được lưu vào thư mục IMG trong project_ver1 của solution1 thành công!");
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một hình ảnh trước khi lưu!");
+            }
         }
 
         private void gunaButton1_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Title = "Chọn Hình ảnh"; 
+            openFileDialog.Title = "Chọn Hình ảnh";
             openFileDialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
             openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                string selectedFilePath = openFileDialog.FileName;
+                selectedFilePath = openFileDialog.FileName;
                 Image selectedImage = Image.FromFile(selectedFilePath);
                 PicProduct.Image = selectedImage;
+                img = selectedImage;
+               
             }
-            // chưa xong
+        }
+        private Image GetImageByName(string imageName)
+        {
+            // Đường dẫn tới thư mục IMG trong thư mục Image của project_ver1
+            string imgFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../Project_ver1/UI/Image");
+            try
+            {
+                // Đường dẫn đến tệp tin ảnh bằng tên hình ảnh
+                string imgFilePath = Path.Combine(imgFolderPath, imageName);
+                // Kiểm tra xem tệp tin ảnh có tồn tại không
+                if (File.Exists(imgFilePath))
+                {
+                    // Tạo một đối tượng Image từ tệp tin ảnh
+                    Image image = Image.FromFile(imgFilePath);
+                    return image;
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy hình ảnh có tên: " + imageName);
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+                return null;
+            }
         }
     }
-    
 }
